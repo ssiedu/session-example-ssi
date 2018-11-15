42a870439c4ae6773a7caec2385eb47556c14814
manoj <>
2018-11-15 19:01:34 +0530

    session-15-11-18

---
+++ src/java/VerifyUser.java
@@ -0,0 +1,75 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class VerifyUser extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String s1=request.getParameter("t1");
+        String s2=request.getParameter("t2");
+        
+        //writing the user id to session of the user
+        
+        //step-1 (fetch the session object)
+        HttpSession session=request.getSession();
+        //step-2 (store the data to session)
+        session.setAttribute("username", s1);
+        
+        
+        
+        out.println("<h3>Welcome "+s1+"</h3>");
+        out.println("<h4><a href=First>Call-First</a></h4>");
+        out.println("<h4><a href=Second>Call-Second</a></h4>");
+        
+        
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <h3>Session-Example</h3>
+        <form action="VerifyUser">
+            <pre>
+            Userid      <input type="text" name="t1"/>
+            Password    <input type="password" name="t2"/>
+                        <input type="submit" value="Login"/>
+            </pre>
+        </form>
+    </body>
+</html>
